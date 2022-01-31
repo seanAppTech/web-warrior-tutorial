3a873bf2a45c7306c30b96ae4b90a10a1242d749
seanAppTech <SStanden@apptechcorp.com>
2022-01-31 15:36:11 -0800

    site

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,36 @@
+import {  graphql, Link } from "gatsby"
 import React from "react"
+import Layout from '../components/Layout'
+import * as styles from '../styles/home.module.css'
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
-export default function Home() {
-  return <div>Hello world!</div>
+export default function Home({ data }) {
+  const image = getImage(data.file.childImageSharp.gatsbyImageData)
+  return (
+    <Layout>
+      <section className={styles.header}>
+        <div>
+          <h2>Design</h2>
+          <h3>Design, Develop, & Deploy</h3>
+          <p>Web designer & developer based in San Diego.</p>
+          <Link className={styles.btn} to="/projects">My Portfolio Projects</Link>
+        </div>
+        <GatsbyImage image={image} alt="Banner" />
+      </section>
+    </Layout>
+  )
 }
+
+export const query = graphql`
+query Banner {
+  file(relativePath: {eq: "banner.png"}) {
+    childImageSharp {
+      gatsbyImageData(
+        layout: FULL_WIDTH
+        placeholder: BLURRED
+        formats: [AUTO, WEBP]
+        )
+    }
+  }
+}
+`
